65ab72c655192ec689f65f808ebe08c298dc2b71
Abdul Dakkak <abduld@wolfram.com>
2020-01-23 08:17:56 -0600

    work

---
+++ Compile.m
@@ -0,0 +1,11 @@
+iCollatz = Function[{Typed[n, "Integer64"]},
+  Module[{m, Typed[list, TypeSpecifier["PackedArray"]["Integer64", 1]] = {n}},
+  m = n;
+  While[m > 1,
+   m = If[Mod[m, 2] === 0, Quotient[m, 2], 3*m + 1];
+   AppendTo[list, m]]; list]
+ ];
+Collatz = FunctionCompile[Collatz];
+
+Print[10 -> Collatz[10]];
+Print[20 -> Collatz[20]];
